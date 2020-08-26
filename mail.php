788f16f062c0a87a78d6dd1291573978bae734f2
mimuro-lab <mimurosyunya@gmail.com>
2020-08-26 18:29:11 +0900

    とりあえずHTMLメールを送信する。 2020/08/26の作業。HTMLを送ることのみは成功。参考サイトは以下、 https://www.pi-pe.co.jp/solution/article/mail/30/

--- mail.php
+++ mail.php
@@ -2,11 +2,45 @@
 
 //とりあえず、このPHPファイルが呼び出されたら送信する
 function sendmail(string $name, string $message){
-    $to = "hitokotosuisen@gmail.com";
-    $subject = "未定のタイトル";
-    $message = $name . "\n" . $message;
-    $header = "From: hitokotosuisen@gmail.cmo" . "\r\n";
-    if(mail($to, $subject, $message, $header)){
+    $mail_from      = 'hitokotosuisen@gmail.com';
+    $mail_to        = 'hitokotosuisen@gmail.com';
+    $mail_from_name = '送信者の名前';
+    
+    $subject = '件名';
+    
+    $body_text = '代替テキストの本文';
+    $body_html = 'HTMLメールの本文';
+    
+    $parameter = "-f ".$mail_from;
+    
+    $boundary = "--".uniqid(rand(),1);
+    
+    // ヘッダー情報
+    $headers .= 'Content-type: text/html;';
+    $headers .= "From: " . $mail_from . "\r\n";
+    
+    // メッセージ部分
+    /*
+    $message = '';
+    $message .= '--' . $boundary . "\r\n";
+    $message .= 'Content-Type: text/plain; charset=UTF-8' . "\r\n";
+    $message .= 'Content-Disposition: inline' . "\r\n";
+    $message .= 'Content-Transfer-Encoding: quoted-printable' . "\r\n";
+    $message .= "\r\n";
+    $message .= quoted_printable_decode ( $body_text ) . "\r\n";
+    $message .= "\r\n";
+    $message .= '--' . $boundary . "\r\n";
+    $message .= 'Content-Type: text/html; charset=UTF-8' . "\r\n";
+    $message .= 'Content-Disposition: inline' . "\r\n";
+    $message .= 'Content-Transfer-Encoding: quoted-printable' . "\r\n";
+    $message .= "\r\n";
+    $message .= quoted_printable_decode ( $body_html ) . "\r\n";
+    $message .= '--' . $boundary . "\r\n";
+    */
+
+    $message = "<h1>".$message."</h1>";
+
+    if(mail($mail_to,$subject, $message, $headers, $parameter)){
         return true;
     }
     
