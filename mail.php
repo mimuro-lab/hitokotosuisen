d33270b80ce7402ca88bf8947fe4de652091f061
mimuro-lab <mimurosyunya@gmail.com>
2020-08-29 15:07:03 +0900

    メール送信の完成。 HTMLメールの実装は後にやる。 とりあえず、テキストベースでのやり取りを行う。

--- mail.php
+++ mail.php
@@ -1,60 +1,47 @@
 <?php
 
 //とりあえず、このPHPファイルが呼び出されたら送信する
-function sendmail(string $name, string $message){
+function sendmailToUser($mail_user){
     $mail_from      = 'hitokotosuisen@gmail.com';
-    $mail_to        = 'hitokotosuisen@gmail.com';
-    $mail_from_name = '送信者の名前';
     
-    $subject = '件名';
-    
-    $body_text = '代替テキストの本文';
-    $body_html = 'HTMLメールの本文';
-    
-    $parameter = "-f ".$mail_from;
-    
-    $boundary = "--".uniqid(rand(),1);
+    $subject = '小山高専図書情報　ひとことすいせん係より';
     
     // ヘッダー情報
-    $headers .= 'Content-type: text/html;';
-    $headers .= "From: " . $mail_from . "\r\n";
+    $headers = "From: ". $mail_from . "\r\n";
     
     // メッセージ部分
-    /*
-    $message = '';
-    $message .= '--' . $boundary . "\r\n";
-    $message .= 'Content-Type: text/plain; charset=UTF-8' . "\r\n";
-    $message .= 'Content-Disposition: inline' . "\r\n";
-    $message .= 'Content-Transfer-Encoding: quoted-printable' . "\r\n";
-    $message .= "\r\n";
-    $message .= quoted_printable_decode ( $body_text ) . "\r\n";
-    $message .= "\r\n";
-    $message .= '--' . $boundary . "\r\n";
-    $message .= 'Content-Type: text/html; charset=UTF-8' . "\r\n";
-    $message .= 'Content-Disposition: inline' . "\r\n";
-    $message .= 'Content-Transfer-Encoding: quoted-printable' . "\r\n";
-    $message .= "\r\n";
-    $message .= quoted_printable_decode ( $body_html ) . "\r\n";
-    $message .= '--' . $boundary . "\r\n";
-    */
+    $message = "
+    ひとことすいせんに参加して頂き、ありがとうございます。
+    このメールに、推薦内容を記載して、返信してください。
 
-    $message = "<h1>".$message."</h1>";
+    記載内容は以下の、記載例のように記載してください。
+    ※推薦者・推薦図書・推薦内容のどれかがない場合は受け付けません。
 
-    if(mail($mail_to,$subject, $message, $headers, $parameter)){
+    #### 記載例 ####
+    推薦者：　　本科　1学年　EE　小山太郎
+
+    推薦図書：　電気回路Ⅰ
+
+    推薦内容：（以下に記載）
+    とても分かり易い！用語の定義から、公式の導出過程まで丁寧に書かれている！
+    また、問題の内容もとても良い！初学者向けの簡単な問題から、編入対策にもなる問題もある！
+    電気回路を総合的に勉強したい人向けの教科書！
+
+    ";
+
+    if(mail($mail_user, $subject, $message, $headers)){
         return true;
     }
     
     return false;
 }
 
+
 // 以下、本文
 $bodyOfMail = $_POST["body"];
 $name = $_POST["name"];
+$mail_to = $_POST["email"];
 
-if(sendmail($name, $bodyOfMail)){
-    echo "メール送信に成功しました!!!";
-}else{
-    echo "残念ながらメール送信はできませんでした。。。";
-}
+sendmailToUser($mail_to);
 
 ?>
