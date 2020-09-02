604a8bb34d112653289d5140e3129e991e477c63
mimuro-lab <mimurosyunya@gmail.com>
2020-09-02 17:35:01 +0900

    ファイルの中身をすべて読み込んでechoする

--- data/read_comment.php
+++ data/read_comment.php
@@ -1,2 +1,32 @@
-<h1>ここでは、投稿されたコメントを読み込みます。</h1>
+<?php
 
+// 存在するファイルの中身を読み込む関数。すべて読み込む。
+function read_from_file_all(String $filename){
+    // ファイルがなかったらリターンする。
+    if(!file_exists($filename)){
+        echo "not exit file (function:read_from_file_all)<br>".$filename;
+        return ;
+    }
+
+    // ファイルを開けなかったらリターンする。
+    if(!fopen($filename, "r")){
+        echo "cannot open file (function:read_from_file_all)";
+        return ;
+    }
+    $fp = fopen($filename, "r");
+    // ファイルの中身を格納する変数
+    $contentOfText = "";
+    while(!feof($fp)){
+
+        // fgetにより一行読み込み
+        $contentOfText .= fgets($fp);
+    }
+        
+    return $contentOfText;
+}
+
+echo read_from_file_all(__DIR__."\comment\sample.txt")
+
+?>
+
+<h1>ここでは、投稿されたコメントを読み込みます。</h1>
