bb69f7e8f4e0b4cb2ad9fb22f34883a82ed1f79a
mimuro-lab <mimurosyunya@gmail.com>
2020-10-08 17:36:44 +0900

    コメント閲覧は３行まで

--- view/utils.php
+++ view/utils.php
@@ -1,47 +1,40 @@
 ﻿<?php
 
-// 存在するファイルの中身を読み込む関数。
-// 読み込むのは、日付・タイトル・タグ・コメント内容
-// 返すのは、[[日付],[タイトル],[タグ],[コメント内容]]の配列
-function read_from_file_all(String $filename){
-  // ファイルがなかったらリターンする。
-  if(!file_exists($filename)){
-    echo "not exit file (function:read_from_file_all)<br>".$filename;
-    return ;
+function getPreviewComment(string $oriComment, int $restLines)
+{
+
+  $listOfComment = explode("<br>", $oriComment);
+  $trimedComment = array();
+  if(count($listOfComment) < $restLines){
+    $restLines = count($listOfComment);
   }
-  
-  // ファイルを開けなかったらリターンする。
-  if(!fopen($filename, "r")){
-    echo "cannot open file (function:read_from_file_all)";
-    return ;
-  }
-  $fp = fopen($filename, "r");
-  // ファイルの中身を格納する配列
-  $contentOfText = array();
-  while(!feof($fp)){
-    // fgetにより一行読み込み
-    $line = fgets($fp);
-    if(count(explode(",", $line)) != 9){
-      continue;
+  $i = 0;
+  foreach($listOfComment as $line){
+    if($i >= $restLines){
+      break;
     }
-    $date = explode(",", $line)[2];
-    $bookAndTag = explode(",", $line)[6];
-    $book = explode(":", $bookAndTag)[0];
-    $tag = str_replace($book.":", "", $bookAndTag);
-    $comment = explode(",", $line)[7];
+    array_push($trimedComment, $line);
+    $i ++;
+  }
   
-    // 文字のエスケープ処理
-    $comment = str_replace("?newl?", "<br>", $comment);
-    $pushLine = array("date"=>$date, "book"=>$book, "tag"=>$tag, "comment"=>$comment);
-    array_push($contentOfText, $pushLine);
-  }      
-  return $contentOfText;
+  // コメントをstrinに戻す
+  $retComment = "";
+
+  for($i = 0; $i < $restLines; $i++){
+    if($i == $restLines - 1){ 
+      $retComment .= $trimedComment[$i];
+    }else{
+      $retComment .= $trimedComment[$i]."<br>";
+    }
+  }
+  
+  return $retComment;
 }
-  
 
 function printHTMLOfComment($listOfContents)
 {
   foreach($listOfContents as $comment){
+    print_r($comment);
     $rinkDate = substr($comment["date"], 0, 10);
     $date = str_replace($rinkDate, "", $comment["date"]);
     echo '
@@ -54,7 +47,7 @@ function printHTMLOfComment($listOfContents)
       <td style="word-break: break-all;"  align="right"><a href="http://localhost:8080/view/?tag='.$rinkDate.'">'.$rinkDate.'</a>'.$date.'</td>
     </tr>
     <tr>
-      <td style="word-break: break-all;"  colspan="2">'.$comment["comment"].'</td>
+      <td style="word-break: break-all;"  colspan="2">'.getPreviewComment($comment["comment"], 3).'</td>
     </tr>
     <tr>
       <td colspan="2"><hr></td>
