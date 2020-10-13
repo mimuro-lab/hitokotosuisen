5b43fd143ca9f180a1e751a79386e7899d949364
mimuro-lab <mimurosyunya@gmail.com>
2020-10-13 13:32:56 +0900

    管理画面の追加。メールアドレスによる二段階認証。

---
+++ admin/success.php
@@ -0,0 +1,42 @@
+﻿<?php
+require_once(__DIR__."\\utils.php");
+print_r($_POST);
+
+if(!isOk($_POST["username"], $_POST["password"])){
+    echo '一致しないユーザー名とパスワードが入力されました。';
+    exit();
+}
+
+$scene = "default";
+
+function main($_scene){
+    switch($_scene){
+        case "default":
+            
+        break;
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>管理者用画面</title>
+    <meta charset="utf-8">
+  </head>
+  <body> 
+  <table width="100%" border="0">
+	<tr><td><br><br></td></tr>
+  <tr>
+      <td align="center" colspan="2">管理者用画面</td>
+	</tr>
+	<tr><td><br><br></td></tr>
+	
+	<table width="100%">
+
+	<?php main($scene)?>
+	
+	</table> 
+  </body>
+</html>
---
+++ admin/utils.php
@@ -0,0 +1,24 @@
+﻿<?php
+function isOk($username, $password)
+{
+	$pathToInfo = __DIR__.'\\..\\data\\admin_userInfo.txt';
+	$fp = fopen($pathToInfo, "r");
+	
+	
+	while($infoLine = fgets($fp)){
+		//　BOM削除
+		$infoLine = preg_replace('/^\xEF\xBB\xBF/', '', $infoLine);
+		
+		if(count(explode(",", $infoLine)) != 3){
+			continue;
+		}
+		$TrueUsername = explode(",", $infoLine)[0];
+		$TruePassword = explode(",", $infoLine)[1];
+		
+		if($username === $TrueUsername && $password === $TruePassword){
+			return True;
+		}
+	}
+	return False;
+}
+?>
